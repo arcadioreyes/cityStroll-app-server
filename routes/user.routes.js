fb05f1ff7e92936032b4e160a73d9baef3af916a
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-07 01:23:29 +0100

    GET  method, /API/USERS - image - route tested and working. Returned value: Current user object

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -15,4 +15,13 @@ router.put("/users", isAuthenticated, (req, res, next) => {
     .catch((err) => console.error(err));
 });
 
+router.get("/users", isAuthenticated, (req, res, next) => {
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and is made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+
+  // Send back the token payload object containing the user data
+  res.status(200).json(req.payload);
+});
+
 module.exports = router;
