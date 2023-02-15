e0079db226502c700dd3cfbe6b5ea7076bf5e0dc
arcadioreyes <109815910+arcadioreyes@users.noreply.github.com>
2023-02-15 01:01:41 +0100

    added lines 32 to 36. Commented out lines 16 and 26 to 30 in order to try deployment

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: ["http://localhost:3000", process.env.ORIGIN],
+      origin: ["http://localhost:3000", `${process.env.ORIGIN}`],
     })
   );
 
